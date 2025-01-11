592c456f84f423326f8fa4781c225df6446e9ff5
Joanna Ohradka <241231@edu.p.lodz.pl>
2025-01-11 14:50:43 +0100

    Added second comment

--- main.py
+++ main.py
@@ -38,5 +38,6 @@ if __name__ == "__main__":
     print(f"Arithmetic operations for A={A}, B={B}")
     print(f"A + B: {adder.Addition(A, B)}")
     print(f"A - B: {diff.Difference(A, B)}")
-    print(f"A * B: {diviser.Division(A, B)}")
+    # Multiplication
+    print(f"A * B: {multi.Multiplication(A, B)}")
     print(f"A / B: {diviser.Division(A, B)}")
