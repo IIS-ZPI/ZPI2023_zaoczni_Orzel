c76c7b95a1db86a86e1aa320e2026363ff6a4618
Joanna Ohradka <241231@edu.p.lodz.pl>
2025-01-15 03:02:56 +0100

    Added render.yaml

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://zpi2024-zaoczni-orzel.onrender.com"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
