d5cd2f58e1fa025eb3cce646fc7aad7e39f72f57
BartlomiejB <blaszczyk17@gmail.com>
2025-02-07 21:56:13 +0100

    Bugfix#96
    Exchange data fetching period outside available dates is now raised as an Error and message is send as a response with status code 400

--- backend/app/domain/services/nbp_service.py
+++ backend/app/domain/services/nbp_service.py
@@ -12,8 +12,8 @@ from app.utils.helpers import calculate_statistics, generate_trend_changes_histo
 logger = logging.getLogger(__name__)
 
 async def fetch_currency_data(request: CurrencyDataRequest) -> CurrencyDataResponse:
-    # Define the cutoff date: data is available only starting from 03-01-2002.
-    cutoff_date = datetime(2002, 1, 2).date()
+    # Define the cutoff date: data is available only starting from 02-01-2002.
+    cutoff_date = datetime(2002, 1, 1).date()
 
     # Validate the request dates.
     if request.endDate == cutoff_date:
@@ -21,7 +21,7 @@ async def fetch_currency_data(request: CurrencyDataRequest) -> CurrencyDataRespo
         logger.error(error_msg)
         raise ValueError(error_msg)
     elif request.endDate > cutoff_date >= request.startDate:
-        error_msg = "No data available for given period because it includes dates before 03-01-2002."
+        error_msg = "No data available for given period because it includes dates before 02-01-2002."
         logger.error(error_msg)
         raise ValueError(error_msg)
 
